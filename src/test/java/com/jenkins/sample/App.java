fc3dfcf3ba84d16d76e4cdc0d047974dfeec261b
aarellivenu@gmail.com <aarellivenu@gmail.com>
2016-07-16 15:40:27 +0530

    New Project is Added.

---
+++ src/test/java/com/jenkins/sample/App.java
@@ -0,0 +1,17 @@
+package com.jenkins.sample;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+  @Test()
+  public void test(){
+	  Assert.assertTrue(true);
+  }
+}
